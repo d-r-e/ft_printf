b1f4655b4324d578d1cbebe8333afc1ec3598317
David Rodriguez <darodrig@c1r3s3.42madrid.com>
2019-12-10 16:05:40 +0100

    makefile

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: darodrig <darodrig@student.42madrid.c      +#+  +:+       +#+        */
+/*   By: darodrig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/09 12:16:24 by darodrig          #+#    #+#             */
-/*   Updated: 2019/12/09 17:58:54 by darodrig         ###   ########.fr       */
+/*   Created: 2019/12/10 15:43:36 by darodrig          #+#    #+#             */
+/*   Updated: 2019/12/10 16:00:33 by darodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,29 @@
 
 typedef struct	s_pf
 {
-	int	printed;
-	int	zero;
-	int	left;
-	int	width;
-	int	prec;
+	int		printed;
+	int		zero;
+	int		left;
+	int		width;
+	int		prec;
 	char	*format;
 	char	*str;
-}		t_pf;
+}				t_pf;
 
-size_t		ft_strlen(const char *s);
-int		ft_printf(const char *s, ...);
-char		*ft_strjoin(const char *s1, const char *s2);
-void		ft_putchar(char c, t_pf *pf);
-void		ft_putstr(const char *str, t_pf *pf);
-void		pf_init(t_pf *pf);
-void		pf_reset(t_pf *pf);
-char		*ft_substr(const char *s, unsigned int start, size_t len);
-int		ft_indexof(const char *s, const char *set);
-int		ft_isdigit(int c);
-void		ft_getformat(const char *f, t_pf *pf);
+size_t			ft_strlen(const char *s);
+int				ft_printf(const char *s, ...);
+
+char			*ft_strjoin(const char *s1, const char *s2);
+void			ft_putchar(char c, t_pf *pf);
+void			ft_putstr(const char *str, t_pf *pf);
+void			pf_init(t_pf *pf);
+void			pf_reset(t_pf *pf);
+char			*ft_substr(const char *s, unsigned int start, size_t len);
+int				ft_indexof(const char *s, const char *set);
+int				ft_isdigit(int c);
+void			ft_getformat(const char *f, t_pf *pf);
+char			*ft_itoa_base(long long nbr, int base);
+char			*ft_strdup(const char *s1);
+char			*ft_itoa(int nbr);
+char			*ft_utoa(unsigned int nbr);
 #endif
