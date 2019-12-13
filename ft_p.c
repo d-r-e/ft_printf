b1ceae8ff21d093e8d66be98f89ae9b5379e7314
David Rodriguez <darodrig@c1r8s3.42madrid.com>
2019-12-13 18:35:41 +0100

    missing * flag

--- ft_p.c
+++ ft_p.c
@@ -6,7 +6,7 @@
 /*   By: darodrig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 11:35:17 by darodrig          #+#    #+#             */
-/*   Updated: 2019/12/12 14:29:57 by darodrig         ###   ########.fr       */
+/*   Updated: 2019/12/13 18:33:04 by darodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_print_pointer(t_pf *pf, va_list arg)
 	char	*spaces;
 
 	pf->str = ft_strjoin("0x", ft_itoa_base((long)va_arg(arg, void*), 16));
+	if (pf->prec == 0)
+		ft_trunc(&pf->str, 2);
 	spaces = ft_spaces(pf->width - ft_strlen(pf->str));
 	if (pf->left == 1)
 	{
